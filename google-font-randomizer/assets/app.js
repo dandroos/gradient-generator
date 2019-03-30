4122378a1e09316f969c97443bf95204a546cd5b
dandroos <dandrewsuk82@gmail.com>
2019-03-30 14:13:11 +0000

    added extra features, incl letter-spacing

--- google-font-randomizer/assets/app.js
+++ google-font-randomizer/assets/app.js
@@ -1,29 +1,44 @@
 let array;
+var toastTimeout;
+var fontTimeout; 
 
-document.addEventListener('keypress', (e)=>{
+const text = document.querySelector('#text');
+
+document.addEventListener('keydown', (e)=>{
     if(e.shiftKey && e.code == 'Space'){
-        updateLink(array[getRandFontIndex(array.length)]);
+        text.classList.add('hide');
+        document.querySelector('#toast').classList.add('hide');
     }
 
     if(e.shiftKey && e.code == 'KeyT'){
         runToast(document.querySelector('#text').style.fontFamily.replace(/"/g, ""));
     }
     
-    if(e.shiftKey && e.code == 'BracketRight'){
-        var text = document.querySelector('#text');
+    if(e.shiftKey && e.keyCode == '38'){
         var size = text.style.fontSize;
         size = parseInt(/\d*/g.exec(size).join());
-        text.style.fontSize = `${(size + 1)}px`;
+        text.style.fontSize = `${size + 1}px`;
     }
 
-    if(e.shiftKey && e.code == 'BracketLeft'){
-        var text = document.querySelector('#text');
+    if(e.shiftKey && e.keyCode == '40'){
         var size = text.style.fontSize;
         size = parseInt(/\d*/g.exec(size).join());
-        text.style.fontSize = `${(size - 1)}px`;
+        text.style.fontSize = `${size - 1}px`;
     }
 
-    if(e.ctrlKey && e.shiftKey && e.code == 'KeyK'){
+    if(e.shiftKey && e.keyCode == '39'){
+        var size = text.style.letterSpacing;
+        size = parseInt(/-?\d*/g.exec(size).join());
+        text.style.letterSpacing = `${size + 1}px`;
+    }
+
+    if(e.shiftKey && e.keyCode == '37'){
+        var size = text.style.letterSpacing;
+        size = parseInt(/-?\d*/g.exec(size).join());
+        text.style.letterSpacing = `${size - 1}px`;
+    }
+
+    if(e.ctrlKey && e.shiftKey && e.code == 'KeyS'){
         var cPanel = document.querySelector('#control-panel');
         cPanel.classList.toggle('hide');
         if(cPanel.classList.contains('hide')){
@@ -32,10 +47,37 @@ document.addEventListener('keypress', (e)=>{
             document.querySelector('#text-input').focus();
         }
     }
+
+    if(e.ctrlKey && e.shiftKey && e.code == 'KeyU'){
+        if(text.style.textTransform == 'uppercase'){
+            text.style.textTransform = 'none';
+        }else{
+            text.style.textTransform = 'uppercase';
+        }
+    }
+
+    if(e.ctrlKey && e.shiftKey && e.code == 'KeyL'){
+        if(text.style.textTransform == 'lowercase'){
+            text.style.textTransform = 'none';
+        }else{
+            text.style.textTransform = 'lowercase';
+        }
+    }
+
+    if(e.ctrlKey && e.shiftKey && e.code == 'KeyN'){
+        text.style.textTransform = 'none';
+    }
 });
 
 document.querySelector('#text-input').addEventListener('keyup', (e)=>{
-    document.querySelector('#text').textContent = e.target.value;
+    text.textContent = e.target.value;
+})
+
+text.addEventListener('transitionend', ()=>{
+    if(text.classList.contains('hide')){
+        text.style.letterSpacing = '0px';
+        updateLink(array[getRandFontIndex(array.length)]);
+    }
 })
 
 function getFonts(){
@@ -46,7 +88,7 @@ function getFonts(){
         }
     }
 
-    xhr.open("GET", "https://www.googleapis.com/webfonts/v1/webfonts?key=", true);
+    xhr.open("GET", "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyAZPt82j4_tyfPLEeWYVp14aXp5SM1-PME", true);
 
     xhr.send();
 }
@@ -78,22 +120,25 @@ function updateLink(font){
 }
 
 function setTextStyle(font){
-    const text = document.querySelector('#text');
     text.style.fontFamily = `'${font}'`;
-    runToast(font)
+    window.clearTimeout(fontTimeout);
+    fontTimeout = setTimeout(()=>{
+        text.classList.remove('hide');
+        runToast(font);
+    }, 800)
+    
 }
 
 function getRandFontIndex(listLength){
     return Math.floor(Math.random() * listLength);
 }
-var t;
+
 function runToast(text){
-    // var t;
     var toast = document.querySelector('#toast');
     toast.textContent = text;
     toast.classList.remove('hide');
-    window.clearTimeout(t);
-    t = window.setTimeout(()=>{
+    window.clearTimeout(toastTimeout);
+    toastTimeout = window.setTimeout(()=>{
         toast.classList.add('hide');
     },5000);
 }
